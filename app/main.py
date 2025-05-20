efa860712a65d7d2be34a4fc38c90003db1eb9b9
roniwellington <roniwellington@gmail.com>
2025-05-20 12:01:04 -0300

    melhorando a formatação e adicionando Docstrings

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI
 from app.routers import routers_produtos, routers_usuarios
 
 
-
-MENSAGEM_HOME: str    ="Bem-vindo à API de Recomendação de Produtos"
+MENSAGEM_HOME: str = "Bem-vindo à API de Recomendação de Produtos"
 
 
 # Criando o App
@@ -13,13 +12,9 @@ app = FastAPI()
 app.include_router(routers_usuarios.router)
 app.include_router(routers_produtos.router)
 
-# Iniciando o servidor
 
+# Iniciando o servidor
 @app.get("/")
 def home() -> Dict[str, str]:
     global MENSAGEM_HOME
     return {"mensagem": MENSAGEM_HOME}
-
-
-
-
